4501a228493f40650eca11ac8acd4b3a9c27629c
Chukwuemeka Uche <chucksilver68@gmail.com>
2025-11-27 22:45:44 +0100

    update on onboarding screen
    update on onboarding screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 import Onboarding from './screens/Onboarding';
 
 const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide(); // 👈 VERY IMPORTANT
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
