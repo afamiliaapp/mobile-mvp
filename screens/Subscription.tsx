87e2d848be229d821efd5fcc9e221b189028c695
Chukwuemeka Uche <chucksilver68@gmail.com>
2026-01-14 21:23:52 +0100

    update on subscription screen
    update on subscription screen

--- screens/Subscription.tsx
+++ screens/Subscription.tsx
@@ -1,12 +1,50 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import AppContainer from '../components/AppContainer';
+import SubscriptionPlanBar from '../components/SubscriptionPlanBar';
+import BackButton from '../components/BackButton';
+import ThemedText from '../components/ThemedText';
 
 export default function Subscription() {
   return (
-    <View>
-      <Text>Subscription</Text>
-    </View>
+    <AppContainer>
+      <View style={styles.container}>
+        <SubscriptionPlanBar />
+        <BackButton />
+
+        <View style={styles.container2}>
+          <ThemedText variant="title" style={styles.cplan}>
+            Current Plan
+          </ThemedText>
+
+          <View style={styles.activeplabox}></View>
+        </View>
+      </View>
+    </AppContainer>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    paddingHorizontal: 20,
+  },
+
+  container2: {
+    height: '85%',
+
+    marginTop: 25,
+  },
+
+  cplan: {
+    fontSize: 16,
+    fontWeight: 500,
+  },
+
+  activeplabox: {
+    backgroundColor: '#2C247A',
+    height: 118,
+    borderRadius: 10,
+    marginTop: 40,
+  },
+});
